8a08e132567333700f60499b1bd08b22fe954ac7
ghr85 <ghrenfrew@gmail.com>
2019-01-24 15:13:05 +0000

    Final commit

--- start_point/star_system.rb
+++ start_point/star_system.rb
@@ -31,22 +31,28 @@ class StarSystem
     @planets.find_all{|planets| planets.number_of_moons == 0}
   end
   def get_planets_with_more_moons(number)
-   number_moon = @planets.find_all{|planets| planets.number_of_moons > number}
-   number_moon.map {|planet| planet.name }
+    number_moon = @planets.find_all{|planets| planets.number_of_moons > number}
+    number_moon.map {|planet| planet.name }
   end
 
- def get_number_of_planets_closer_than(distance)
+  def get_number_of_planets_closer_than(distance)
     number_distance= @planets.find_all{|planets| planets.distance_from_sun < distance}
-   number_distance.length
- end
+    number_distance.length
+  end
 
- def get_total_number_of_moons
-   @planets.reduce(0){|sum,moons| sum += moons.number_of_moons}
- end
+  def get_total_number_of_moons
+    @planets.reduce(0){|sum,moons| sum += moons.number_of_moons}
+  end
 
- def get_planet_names_sorted_by_increasing_distance_from_sun
-sorted_planets = @planets.sort{|distance_1,distance_2| distance_1.distance_from_sun <=> distance_2.distance_from_sun}
-sorted_names = sorted_planets.map{|planet| planet.name}
-return sorted_names
- end
+  def get_planet_names_sorted_by_increasing_distance_from_sun
+    sorted_planets = @planets.sort{|distance_1,distance_2| distance_1.distance_from_sun <=> distance_2.distance_from_sun}
+    sorted_names = sorted_planets.map{|planet| planet.name}
+    return sorted_names
+  end
+
+  def get_planet_names_sorted_by_size_decreasing
+    sorted_planets = @planets.sort{|diameter_1,diameter_2| diameter_2.diameter <=> diameter_1.diameter}
+    sorted_names = sorted_planets.map{|planet| planet.name}
+    return sorted_names
+  end
 end
