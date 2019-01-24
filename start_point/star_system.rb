3d48f925eefd04f92ed9bd8dfd9133c52424bf6f
ghr85 <ghrenfrew@gmail.com>
2019-01-24 14:45:17 +0000

    First few tests passing, note use of comparators

--- start_point/star_system.rb
+++ start_point/star_system.rb
@@ -1,5 +1,31 @@
 class StarSystem
   attr_reader :name, :planets
 
-    
+  def initialize(name, planets)
+    @name = name
+    @planets = planets
+  end
+
+  def planet_names
+    planet_names = @planets.map do |planet_element|
+      planet_element.name
+
+    end
+    return planet_names
+  end
+
+  def get_planet_by_name(planet_name)
+    @planets.find{|planet| planet.name == planet_name}
+  end
+
+  def get_largest_planet
+    @planets.max{|big_planet,planet| big_planet.diameter <=> planet.diameter}
+
+  end
+
+  def get_smallest_planet
+    @planets.min{|small_planet,planet| small_planet.diameter <=> planet.diameter}
+
+  end
+
 end
